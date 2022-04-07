29fe45aece44012b7da356171e886d31cf32ba9e
Youme01 <samiya.kabir@northsouth.edu>
2022-04-07 17:10:02 +0600

    audio recitation implemented

--- app/src/main/java/com/example/alquranapp/activities/SurahDetailsActivity.java
+++ app/src/main/java/com/example/alquranapp/activities/SurahDetailsActivity.java
@@ -6,11 +6,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.alquranapp.R;
@@ -22,6 +30,7 @@ import com.example.alquranapp.model.SurahDetail;
 import com.example.alquranapp.viewmodel.SurahDetailViewModel;
 import com.example.alquranapp.viewmodel.SurahViewModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,8 +45,14 @@ public class SurahDetailsActivity extends AppCompatActivity {
     private String eng = "english_rwwad";
     private EditText search;
     private ImageView settings;
-
-
+    private String qariAB = "abdul_basit_murattal";
+    private String qr ;
+    private String str;
+    Handler handler = new Handler();
+    SeekBar seekBar;
+    TextView startTime, totalTime;
+    ImageButton play;
+    MediaPlayer mediaPlayer;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -46,6 +61,7 @@ public class SurahDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_surah_details);
 
         init();
+
         no = getIntent().getIntExtra(common.SURAH_NO,0);
         surahName.setText(getIntent().getStringExtra(common.SURAH_NAME));
 
@@ -54,13 +70,21 @@ public class SurahDetailsActivity extends AppCompatActivity {
 
         surahTranslation.setText(getIntent().getStringExtra(common.SURAH_TRANSLATION));
 
+
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
         recyclerView.setHasFixedSize(true);
         list = new ArrayList<>();
-
         SurahTranslation(eng,no);
+
+
+        try {
+            listenAudio(qariAB);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         search.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -79,7 +103,6 @@ public class SurahDetailsActivity extends AppCompatActivity {
         });
 
     }
-
 
 
     private void init(){
@@ -134,5 +157,161 @@ public class SurahDetailsActivity extends AppCompatActivity {
             }
         }
         surahDetailAdapter.filter(arrayList);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void listenAudio(String qariAB) throws IOException {
+
+        ColorStateList sl = ColorStateList.valueOf( Color.YELLOW);
+        play = findViewById(R.id.play);
+        startTime = findViewById(R.id.start_time);
+        totalTime = findViewById(R.id.total_time);
+        seekBar = findViewById(R.id.seekbar);
+        seekBar.setSecondaryProgressTintList(sl);
+        seekBar.setProgressBackgroundTintList(sl);
+
+        mediaPlayer = new MediaPlayer();
+        seekBar.setMax(100);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){
+                    handler.removeCallbacks(updater);
+                    mediaPlayer.pause();
+                    play.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+                }else{
+                    mediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24);
+                    updateSeekbar();
+                }
+            }
+        });
+        
+        preparedMediaPlayer(qariAB);
+
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                SeekBar seekBar = (SeekBar) view;
+                int playPosition = (mediaPlayer.getDuration()/100)*seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                startTime.setText(timeToMilliSec(mediaPlayer.getCurrentPosition()));
+
+                return false;
+            }
+        });
+
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                seekBar.setSecondaryProgress(i);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                seekBar.setProgress(0);
+                play.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+                startTime.setText("0.00");
+                totalTime.setText("0.00");
+                mediaPlayer.reset();
+                try {
+                    preparedMediaPlayer(qariAB);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    private void preparedMediaPlayer(String qariAB) throws IOException {
+
+        if(no<10){
+           str = "00" + no;
+        }else if(no<100){
+            str = "0" + no;
+        }else if(no>=100){
+            str = String.valueOf(no);
+        }
+        //https://download.quranicaudio.com/quran/abdul_basit_murattal/001.mp3
+        mediaPlayer.setDataSource("https://download.quranicaudio.com/quran/"+qariAB+"/"+str.trim()+".mp3");
+        mediaPlayer.prepare();
+        totalTime.setText(timeToMilliSec(mediaPlayer.getDuration()));
+    }
+
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekbar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            startTime.setText(timeToMilliSec(currentDuration));
+        }
+    };
+
+    private void updateSeekbar(){
+
+        if(mediaPlayer.isPlaying()){
+            seekBar.setProgress((int) (((float)mediaPlayer.getCurrentPosition()/mediaPlayer.getDuration())*100));
+            handler.postDelayed(updater,1000);
+        }
+    }
+
+    private String timeToMilliSec(long ms){
+
+        String timeString = "";
+        String secondString;
+
+        int hours = (int) (ms/(1000*60*60));
+        int min = (int)(ms%(100*60*60))/(1000*60);
+        int sec = (int)((ms%(100*60*60)) % (1000*60)/1000);
+
+        if(hours >0){
+            timeString = hours + ":";
+        }
+        if(sec<10){
+           secondString = "0" + sec;
+        }else{
+            secondString = "" + sec;
+        }
+        timeString = timeString + min + ":" +secondString;
+
+        return timeString;
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer.isPlaying()){
+            handler.removeCallbacks(updater);
+            mediaPlayer.pause();
+            play.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        if(mediaPlayer.isPlaying()){
+            handler.removeCallbacks(updater);
+            mediaPlayer.pause();
+            play.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        if(mediaPlayer.isPlaying()){
+            handler.removeCallbacks(updater);
+            mediaPlayer.pause();
+            play.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+        }
+        super.onPause();
     }
 }
